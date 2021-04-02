ae38b5bf6a942e761d630cfb1254595eedaddfdd
mahesh <mahesh303@hotmail.com>
2021-04-01 22:10:32 -0400

    Initial Commit

---
+++ git test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  git test
+//
+//  Created by Mahesh Patel on 4/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ git test/git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_testApp.swift
+//  git test
+//
+//  Created by Mahesh Patel on 4/1/21.
+//
+
+import SwiftUI
+
+@main
+struct git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
